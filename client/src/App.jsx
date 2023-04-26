9a4038af16104db065e5342a72298eb3a86448a9
Ruchi Mandliya <rmandliya18@gmail.com>
2023-04-26 21:49:39 +0530

    create Navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
+  const theme = useMemo(() => createTheme(themeSettings(), []));
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </ThemeProvider>
+    </>
+  );
+};
 
-export default App
+export default App;
